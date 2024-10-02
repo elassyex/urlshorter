6a4586e7a5ab662f65981342d1350a71e1b25b82
Mohamed Elassy <103861551+elassyex@users.noreply.github.com>
2024-10-02 15:00:58 +0300

    Update views.py

--- urlshortin/views.py
+++ urlshortin/views.py
@@ -3,21 +3,30 @@ from django.http import HttpResponse
 import uuid
 from .models import Make
 
+global mao
 def home(request):
 
     return render(request, 'base/index.html')
-
+mao = {"link":"",
+      "uuid":""}
 def create(request):
+    global mao
     if request.method == 'POST':
         link = request.POST['link']
         if 'https://' not in link:
             link = 'https://' + link
             
         uid = str(uuid.uuid4())[:5]
-        newmake = Make(link=link, uuid=uid)
-        newmake.save()
+        mao ={"link":link,
+             "uuid":uid}
+        # newmake = Make(link=link, uuid=uid)
+        # newmake.save()
         return HttpResponse(uid)
 
 def go(request, pk):
-    urla = Make.objects.get(uuid=pk)
-    return redirect(urla.link)
+    global mao
+    for key, val in mao.items():
+        if key == 'uuid' and val == str(pk):
+            urla=mao.get('link')
+    #urla = mao.get(uuid=pk)
+    return redirect(urla)
